402df83a9a3e3b08119a580a957ee9bf9092532a
Alexander Schieving <alexanderschieving@gmail.com>
2017-09-04 15:09:53 +0200

    Created song model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.create!([ { title: "Go with the Flow", album: "Songs for the Deaf", release_year: 2002,}, 
+{ title: "Everlong", album: "The Colour and the Shape", release_year: 1997,},
+{ title: "Plug in Baby", album: "Origin of Symmetry", release_year: 2001}, ])
+
+songs_count = Song.all.length
+puts "#{songs_count} songs were created."
