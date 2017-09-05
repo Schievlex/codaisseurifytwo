56f5867edee0a58cf4ad8088e0d30c64912cfdb0
Alexander Schieving <alexanderschieving@gmail.com>
2017-09-05 10:16:34 +0200

    Add Song validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,16 @@
 class Song < ApplicationRecord
+
+	validates :title, presence: true
+  	validates :title, uniqueness: true
+
+  	validates :album, presence: true
+  	validates :album, uniqueness: true
+
+  	validates :release_year, length: { maximum: 4 }, allow_blank: true
+
 	def self.order_by_name
     order(:name)
   	end
+
+
 end
