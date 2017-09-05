b10d660d57e97853b397e1f100119bf94be5f41f
Alexander Schieving <alexanderschieving@gmail.com>
2017-09-05 12:07:23 +0200

    Edit, update and delete artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,15 +12,43 @@ class ArtistsController < ApplicationController
   	end
 
   	def create
-    artist_params = params.require(:artist).permit(:name, :image_url)
+   
 
-	    @artist = Artist.new(artist_params)
+    @artist = Artist.new(artist_params)
 
 	    if @artist.save
 	       redirect_to @artist
 	    else
 	       render 'new'
 	    end
+  	end
+
+  	def edit
+    @artist = Artist.find(params[:id])
+  	end
+
+  	def update
+  		@artist = Artist.find(params[:id])
+
+	    if @artist.update_attributes(artist_params)
+	       redirect_to @artist
+	    else
+	       render 'edit'
+	    end
+  	end
+
+  	def destroy
+    @artist = Artist.find(params[:id])
+
+    @artist.destroy
+
+    redirect_to artists_path
+  end
+
+  	private
+
+  	def artist_params
+    params.require(:artist).permit(:name, :image_url)
   	end
 
 end
