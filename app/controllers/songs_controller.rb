f8d17d579867d60715deedd0e1f4012123722bbd
Alexander Schieving <alexanderschieving@gmail.com>
2017-09-05 09:51:34 +0200

    Create the show and new song pages

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,5 +2,26 @@ class SongsController < ApplicationController
 	def index
 		@songs = Song.all
 	end
+
+	def show
+    	@song = Song.find(params[:id])
+    end
+
+    def new
+    @song = Song.new
+  	end
+
+  	def create
+    song_params = params.require(:song).permit(:title, :album, :release_year)
+
+    @song = Song.new(song_params)
+
+	    if @song.save
+	       redirect_to @song
+	    else
+	       render 'new'
+	    end
+  	end
+
 end
 	
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   root "songs#index"
 
   get "songs" => "songs#index"
+  get "songs/new" => "songs#new", as: :new_song
+  get "songs/:id" => "songs#show", as: :song
+
+  post "songs" => "songs#create"
 end
