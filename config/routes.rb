28c5ed915fc6e6a40b52aaacf44de039deae5756
Alexander Schieving <alexanderschieving@gmail.com>
2017-09-05 11:48:32 +0200

    Create show and new artist pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "songs#index"
+  #root "songs#index"
+  root "artists#index"
 
   resources :songs
   resources :artists
